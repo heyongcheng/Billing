24230b30e3484eaac1b0c2fcb21533df20e1f2fb
heyongcheng <178831731@qq.com>
2016-03-26 16:00:50 +0800

    Intellij 提交测试

--- Billing/src/main/java/com/he/utils/Test.java
+++ Billing/src/main/java/com/he/utils/Test.java
@@ -2,7 +2,7 @@ package com.he.utils;
 
 public class Test {
 	public static void main(String[] args) {
-		String s = "ab8&^cd43fsfsaewhkhk2";
+		String s = "12qwaszx";
 		String md5 = Security.MD5(s);
 		System.out.println(md5);
 	}
