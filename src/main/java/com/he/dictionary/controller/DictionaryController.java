f78af23d7470dc13e0d023007cfc44e7fd476986
heyongcheng <178831731@qq.com>
2016-03-26 19:42:02 +0800

    本地提交

---
+++ src/main/java/com/he/dictionary/controller/DictionaryController.java
@@ -0,0 +1,13 @@
+package com.he.dictionary.controller;
+
+import com.he.base.BaseModel;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by HE on 2016/3/26.
+ */
+@Controller("/dictionary")
+public class DictionaryController extends BaseModel{
+    
+
+}
