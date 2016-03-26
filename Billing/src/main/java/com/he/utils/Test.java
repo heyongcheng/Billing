22ca6552d9e29b0d8e651abb8d2ad00ddfce9042
heyongcheng <178831731@qq.com>
2016-03-26 16:09:35 +0800

    测试

--- Billing/src/main/java/com/he/utils/Test.java
+++ Billing/src/main/java/com/he/utils/Test.java
@@ -3,6 +3,7 @@ package com.he.utils;
 public class Test {
 	public static void main(String[] args) {
 		String s = "12qwaszx";
+        String s2 = "123";
 		String md5 = Security.MD5(s);
 		System.out.println(md5);
 	}
