cc68c0ea2900a49545f3685d05e6d5482e35b57c
heyongcheng <178831731@qq.com>
2016-03-31 23:10:28 +0800

    @JsonIgnore 过滤User序列化属性

--- src/main/java/com/he/user/model/User.java
+++ src/main/java/com/he/user/model/User.java
@@ -3,6 +3,7 @@ package com.he.user.model;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.he.base.BaseModel;
 import com.he.org.model.Org;
 import com.he.role.model.Role;
@@ -17,7 +18,7 @@ public class User extends BaseModel{
     private Long orgId;
 
     private String account;
-
+    @JsonIgnore
     private String password;
 
     private String name;
@@ -33,32 +34,35 @@ public class User extends BaseModel{
     private Byte sex;
     
     private Byte status;
-
+    @JsonIgnore
     private Byte deleteFlag;
     /**用户辅助组织列表**/
+    @JsonIgnore
     private List<Org> supplyOrgs;
     /**用户当前登录组织**/
+    @JsonIgnore
     private Org currentOrg;
     /**用户角色列表**/
+    @JsonIgnore
     private List<Role> roles;
 
     private Date createTime;
 
     private Date updateTime;
-
+    @JsonIgnore
     private String createBy;
-
+    @JsonIgnore
     private String updateBy;
-
+    @JsonIgnore
     private String deleteBy;
-
+    @JsonIgnore
     private String reserved1;
-
+    @JsonIgnore
     private String reserved2;
-
+    @JsonIgnore
     private String reserved3;
-
+    @JsonIgnore
     private String reserved4;
-
+    @JsonIgnore
     private String reserved5;
 }
