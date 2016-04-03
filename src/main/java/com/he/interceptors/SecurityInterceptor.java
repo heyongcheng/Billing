7698db3afd5204e0a31f4ec12551cd939dcf9727
heyongcheng <178831731@qq.com>
2016-04-03 11:21:45 +0800

    本地提交

--- src/main/java/com/he/interceptors/SecurityInterceptor.java
+++ src/main/java/com/he/interceptors/SecurityInterceptor.java
@@ -10,45 +10,46 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class SecurityInterceptor implements HandlerInterceptor{
-	
-	private List<String> excludeUrl;
-	public void setExcludeUrl(List<String> excludeUrl) {
-		this.excludeUrl = excludeUrl;
-	}
-	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		String url = request.getRequestURI().replace(request.getContextPath(), "");
-		//不拦截地址
-		if(url.startsWith("/js/") || url.startsWith("/dictionary/")){
-			return true;
-		}
-		if(excludeUrl != null && !excludeUrl.isEmpty()){
-			Iterator<String> iterator = excludeUrl.iterator();
-			while(iterator.hasNext()){
-				if(url.contains(iterator.next())){
-					return true;
-				}
-			}
-		}
-		if(request.getSession().getAttribute("loginUser") == null){
-			request.setAttribute("loginTimout", "登录超时，请重新登录");
-			return false;
-		}
-		return true;
-	}
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		
-	}
+    private List<String> excludeUrl;
+    public void setExcludeUrl(List<String> excludeUrl) {
+        this.excludeUrl = excludeUrl;
+    }
 
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		
-	}
-	
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String url = request.getRequestURI().replace(request.getContextPath(), "");
+        //不拦截地址
+        if(url.startsWith("/js/") || url.startsWith("/dictionary/")){
+            return true;
+        }
+        if(excludeUrl != null && !excludeUrl.isEmpty()){
+            Iterator<String> iterator = excludeUrl.iterator();
+            while(iterator.hasNext()){
+                if(url.contains(iterator.next())){
+                    return true;
+                }
+            }
+        }
+        if(request.getSession().getAttribute("loginUser") == null){
+            request.setAttribute("msg", "登录超时，请重新登录");
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+
+    }
+
 }
